46aef7471725571c889dc9108cb9490262960bc4
theonlybasit <olawaletemitope63@gmail.com>
2026-05-19 11:53:15 +0100

    Implemented CORS options config

--- src/server.js
+++ src/server.js
@@ -5,8 +5,29 @@ const connectDB = require('./config/db.js')
 const cors = require('cors')
 
 const app = express()
+
+const allowedOrigins = [process.env.CLIENT_URL || 'http://localhost:3000'];
+
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (!origin) {
+            return callback(null, true);
+        }
+
+        if (allowedOrigins.includes(origin)) {
+            return callback (null, true);
+        } else {
+            return callback (new Error('Blocked by CORS. Origin not allowed'));
+        }
+    },
+    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+};
+
+
 app.use(express.json())
-app.use(cors())
+app.use(cors(corsOptions))
 
 connectDB()  
 
